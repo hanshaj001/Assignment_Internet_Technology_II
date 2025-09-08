a47f7206b0466990ec3873f6b941e3d4a05466ba
Hansh Raj Joshi <joshihanshraj635@gmail.com>
2025-09-08 07:25:58 +0545

    removed html

--- Assignment I/constant.php
+++ Assignment I/constant.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Constant</title>
-</head>
-<body>
+
     <?php
     define("SITE_NAME","Pokhara University");
     echo SITE_NAME ;
     ?>
-</body>
-</html>
--- Assignment I/intro.php
+++ Assignment I/intro.php
@@ -1,39 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-   <form action="intro.php" method="post">
-
-    <label for="name">Enter Your name :
-        <input type="text" name="name" id="name">
-    </label> <br> <br>
-
-    <label for="name">Enter Your age :
-        <input type="text" name="age" id="age">
-    </label> <br><br>
-
-    <label for="name">Enter Your program :
-        <input type="text" name="program" id="program">
-    </label><br><br>
-
-    <input type="submit">
-   </form>
-
-   <!-- php  -->
 
    <?php 
 
-   $name = $_POST["name"];
-   $age = $_POST["age"];
-   $program = $_POST["program"];
+   $name = "Hansh";
+   $age = 25 ;
+   $program = "BCSIT";
 
    echo "<br><b> My name is $name, I am $age years old. I am Studying $program</b>"
 
    ?>
-</body>
-</html>
